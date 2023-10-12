f2ee9716f9c12e011da41814530cd1a22efbb6a2
Ayush Singh <99062720+ayush-singh05@users.noreply.github.com>
2023-10-12 11:03:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,12 +10,24 @@ function convertToRoman(num) {
     };
 
   //your code here
-
+	return IntegerToRoman(num);
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
 // console.log(convertToRoman(36));
-
+function IntegerToRoman(num) {
+  let value = [1000,900,500,400,100,90,50,40,10,9,5,4,1];
+let roman = ['M','CM','D','CD','C','XC','L','XL','X','IX','V','IV','I'];
+let n = 14;
+let ans = "";
+for(let i = 0; i < value.length; i++){
+  while(n >= value[i]){
+    ans += roman[i];
+    n-=value[i];
+  }
+}
+return ans;
+}
 
 
 
