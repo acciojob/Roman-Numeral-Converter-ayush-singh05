21b5ef1a57f5f8b5fdc1773914aafc5c01939cb0
Ayush Singh <99062720+ayush-singh05@users.noreply.github.com>
2023-10-12 11:29:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -18,12 +18,12 @@ function convertToRoman(num) {
 function IntegerToRoman(num) {
   let value = [1000,900,500,400,100,90,50,40,10,9,5,4,1];
 let roman = ['M','CM','D','CD','C','XC','L','XL','X','IX','V','IV','I'];
-let n = 14;
+
 let ans = "";
 for(let i = 0; i < value.length; i++){
-  while(n >= value[i]){
+  while(num >= value[i]){
     ans += roman[i];
-    n-=value[i];
+    num-=value[i];
   }
 }
 return ans;
